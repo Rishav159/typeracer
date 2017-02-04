5725c6f1000b630253c44b33a98310669cad904a
Rishav Agarwal <rishav.159@gmail.com>
2017-02-04 19:15:37 +0530

    Added set timer routes in socket back end and updated gitignore

--- routes/socket.js
+++ routes/socket.js
@@ -1,11 +1,61 @@
 var prepareSocket = function(socket){
-  // socket.emit('msg',"Welcome to the room");
+
   console.log("A socket connected")
+
+  var getTimeRemaining = function(endtime){
+    var now = new Date();
+    var t = Date.parse(endtime) - Date.parse(now);
+    var seconds = Math.floor( (t/1000) % 60 );
+    var minutes = Math.floor( (t/1000/60) % 60 );
+    var hours = Math.floor( (t/(1000*60*60)) % 24 );
+    var days = Math.floor( t/(1000*60*60*24) );
+
+    if(t<=0){
+      return {
+        'total': 0,
+        'days': 0,
+        'hours': 0,
+        'minutes': 0,
+        'seconds': 0
+      };
+    }else{
+      return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+      };
+    }
+  }
+  var initializeClock = function(endtime){
+    function updateClock(){
+      var t = getTimeRemaining(endtime);
+      if(t.total<=0){
+        clearInterval(timeinterval);
+        socket.broadcast.emit('start_game',{})
+      }
+    }
+    updateClock()
+    var timeinterval = setInterval(updateClock,1000);
+  }
+
+
+  socket.on('set_timer',function(){
+    var t = new Date();
+    t.setSeconds(t.getSeconds()+5);
+    initializeClock(t);
+    socket.broadcast.emit('timer_is_set',t)
+  })
+
+
   socket.on('msg',function(data){
     socket.broadcast.emit('msg',data)
   });
+
   socket.on('disconnect',function(){
     console.log("Bye Bye");
   });
+  
 }
 module.exports = prepareSocket
